50865aed2e6735d4ae5f9c51bf4c65eacdcf300d
unknown <abhishekanantharam123@gmail.com>
2020-07-20 12:48:03 +0530

    searching code

---
+++ Linked List/Searching.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+class node{
+    public:
+        int data;
+        node*next;
+        node(int d){
+            data = d;
+        }
+};
+
+void inserthead(node*&head,int d){
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+void inserttail(node*head,int d){
+    while(head->next!=NULL){
+        head=head->next;
+    }
+    node *n = new node(d);
+    head->next = n;
+    n->next=NULL;
+}
+
+void printnode(node*head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head = head->next;
+
+    }
+}
+
+void searchnodeiterative(node*head,int d){
+    while(head!=NULL){
+        if(head->data==d){
+            cout<<"YES"<<endl;
+            return;
+        }
+        head=head->next;
+    }
+    cout<<"NO"<<endl;
+    return;
+}
+
+void searchnoderecursive(node*head,int d){
+    if(head==NULL){
+        cout<<"NO"<<" ";
+        return;
+    }
+
+    if(head->data==d){
+        cout<<"YES"<<" ";
+        return;
+    }
+    searchnoderecursive(head->next,d);
+}
+
+void buildll(node*&head){
+    int ip;
+    cin>>ip;
+    while(ip!=-1){
+        if(head==NULL){
+            inserthead(head,ip);
+        }
+        else{
+        inserttail(head,ip);
+        
+        }
+        cin>>ip;
+        }
+    return;
+    }
+    
+
+
+int main(){
+    node *head = NULL;
+    buildll(head);
+    printnode(head);
+    cout<<endl;
+    searchnodeiterative(head,5);
+    searchnodeiterative(head,7);
+    searchnoderecursive(head,4);
+    searchnoderecursive(head,-1);
+}
