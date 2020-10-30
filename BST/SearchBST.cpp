769259fb426c7625372f948837fb4bc143f2ded2
unknown <abhishekanantharam123@gmail.com>
2020-10-30 23:48:57 +0530

    SEARCH IN BST

---
+++ BST/SearchBST.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+node * buildbst(node *root,int d){
+
+    if(root==NULL){
+        return new node(d);
+    }
+
+    if(d>root->data){
+        root->right = buildbst(root->right,d);
+    }
+    else{
+        root->left = buildbst(root->left,d);
+    }
+
+    return root;
+}
+
+void bfs(node *root){
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node *t=q.front();
+        
+        if(t==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+         }
+        else{
+            cout<<t->data<<" ";
+            q.pop();
+         if(t->left){
+            q.push(t->left);
+        }
+        if(t->right){
+            q.push(t->right);
+        }
+    }
+    }
+}
+
+void inorder(node *root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+node *search(node *root,int d){
+    node *ans;
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==d){
+        return root;
+    }
+    if(d>root->data){
+        ans=search(root->right,d);
+    }
+    else{
+        ans=search(root->left,d);
+    }
+    return ans;
+}
+
+int main() {
+    node * root=NULL;
+    int arr[]={5,3,7,1,6,8};
+    for(auto it:arr){
+       root= buildbst(root,it);
+    }
+    inorder(root);
+    cout<<endl;
+    bfs(root);
+    node*ans=search(root,0);
+    if(ans!=NULL)
+    cout<<ans->data;
+    else{
+        cout<<"NO";
+    }
+
+}
